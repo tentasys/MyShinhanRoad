daff446acd18b6d21a9c5a7c6ab472151bfeed51
refuse1993 <refuse1993@gmail.com>
2019-08-22 09:23:31 +0900

    BackgroundTask Class Add
    Http 프로토콜을 통한 result 클래스를 외부 클래스로 생성하여 활용

---
+++ Shinple/app/src/main/java/com/example/shinple/BackgroundTask.java
@@ -0,0 +1,69 @@
+package com.example.shinple;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class BackgroundTask extends AsyncTask<Void, Void, String> {
+
+    String target;
+
+    public BackgroundTask(String target) {
+        super();
+        this.target = target;
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+
+        try{
+            URL url = new URL(target);//URL 객체 생성
+
+            //URL을 이용해서 웹페이지에 연결하는 부분
+            HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+
+            //바이트단위 입력스트림 생성 소스는 httpURLConnection
+            InputStream inputStream = httpURLConnection.getInputStream();
+
+            //웹페이지 출력물을 버퍼로 받음 버퍼로 하면 속도가 더 빨라짐
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String temp;
+
+            //문자열 처리를 더 빠르게 하기 위해 StringBuilder클래스를 사용함
+            StringBuilder stringBuilder = new StringBuilder();
+
+            //한줄씩 읽어서 stringBuilder에 저장함
+            while((temp = bufferedReader.readLine()) != null){
+                stringBuilder.append(temp + "\n");//stringBuilder에 넣어줌
+            }
+
+            //사용했던 것도 다 닫아줌
+            bufferedReader.close();
+            inputStream.close();
+            httpURLConnection.disconnect();
+            return stringBuilder.toString().trim();//trim은 앞뒤의 공백을 제거함
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        if(result != null) {
+            Log.d("WHYWHY", result);
+        }
+    }
+}
--- Shinple/app/src/main/java/com/example/shinple/CourseListFragment.java
+++ Shinple/app/src/main/java/com/example/shinple/CourseListFragment.java
@@ -15,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.ListView;
 
 import com.example.shinple.VO.CourseVO;
+import com.example.shinple.BackgroundTask;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -103,7 +104,9 @@ public class CourseListFragment extends Fragment{
         adapter.setOnItemClickListener(new CourseAAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(View view, String courseName, String courseInfo) {
-                new CourseListFragment.BackgroundTask().execute();
+                //new CourseListFragment.BackgroundTask().execute();
+                BackgroundTask backgroundTask = new BackgroundTask("https://192.168.1.187/lectureList.php");
+                backgroundTask.execute();
                 ((MainActivity) view.getContext())
                         .getSupportFragmentManager()
                         .beginTransaction()
@@ -114,7 +117,8 @@ public class CourseListFragment extends Fragment{
         adapter2.setOnItemClickListener(new CourseAAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(View view, String courseName, String courseInfo) {
-                new CourseListFragment.BackgroundTask().execute();
+                BackgroundTask backgroundTask = new BackgroundTask("https://192.168.1.187/lectureList.php");
+                backgroundTask.execute();
                 ((MainActivity) view.getContext())
                         .getSupportFragmentManager()
                         .beginTransaction()
@@ -125,7 +129,8 @@ public class CourseListFragment extends Fragment{
         adapter3.setOnItemClickListener(new CourseAAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(View view, String courseName, String courseInfo) {
-                new CourseListFragment.BackgroundTask().execute();
+                BackgroundTask backgroundTask = new BackgroundTask("https://192.168.1.187/lectureList.php");
+                backgroundTask.execute();
                 ((MainActivity) view.getContext())
                         .getSupportFragmentManager()
                         .beginTransaction()
@@ -135,65 +140,4 @@ public class CourseListFragment extends Fragment{
         });
         return v;
     }
-
-
-    class BackgroundTask extends AsyncTask<Void, Void, String> {
-        String target;
-
-        @Override
-        protected void onPreExecute() {
-            //List.php은 파싱으로 가져올 웹페이지
-            target = "http://192.168.1.187/lectureList.php";
-        }
-
-        @Override
-        protected String doInBackground(Void... voids) {
-
-            try{
-                URL url = new URL(target);//URL 객체 생성
-
-                //URL을 이용해서 웹페이지에 연결하는 부분
-                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
-
-                //바이트단위 입력스트림 생성 소스는 httpURLConnection
-                InputStream inputStream = httpURLConnection.getInputStream();
-
-                //웹페이지 출력물을 버퍼로 받음 버퍼로 하면 속도가 더 빨라짐
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                String temp;
-
-                //문자열 처리를 더 빠르게 하기 위해 StringBuilder클래스를 사용함
-                StringBuilder stringBuilder = new StringBuilder();
-
-                //한줄씩 읽어서 stringBuilder에 저장함
-                while((temp = bufferedReader.readLine()) != null){
-                    stringBuilder.append(temp + "\n");//stringBuilder에 넣어줌
-                }
-
-                //사용했던 것도 다 닫아줌
-                bufferedReader.close();
-                inputStream.close();
-                httpURLConnection.disconnect();
-                return stringBuilder.toString().trim();//trim은 앞뒤의 공백을 제거함
-
-            }catch(Exception e){
-                e.printStackTrace();
-            }
-            return null;
-
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            if(result != null) {
-                Log.d("WHYWHY", result);
-            }
-        }
-    }
-
 }
