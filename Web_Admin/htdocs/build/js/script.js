77327b8ba3734540f5bb41be800cb08c1107b4a8
unknown <kye950308@gmail.com>
2019-08-26 11:02:54 +0900

    다희언니 잘받아

---
+++ Web_Admin/htdocs/build/js/script.js
@@ -0,0 +1,47 @@
+function study_write() {
+    location.href = "study_write.html"
+}
+
+function test_write(){
+    locataion.href ="test_write.html"
+}
+
+function study_submit() {
+    location.href = "study_list.html"
+}
+
+function study_list() {
+    location.href = "study_list.html"
+}
+
+function notice_write() {
+    location.href = "notice_write.html"
+}
+
+function notice_list() {
+    location.href = "notice_list.html"
+}
+
+function qna_write() {
+    location.href = "qna_write.html"
+}
+
+function qna_list() {
+    location.href = "qna_list.html"
+}
+
+function faq_write() {
+    location.href = "faq_write.html"
+}
+
+function faq_list() {
+    location.href = "faq_list.html"
+}
+
+function study_test() {
+    location.href ="study_test.html"
+}
+
+function study_key() {
+    location.href = "study_key.html"
+}
